1bc9fd6c07b7d55fa520a94ac0784f87c2820216
thisisminh172 <65597101+thisisminh172@users.noreply.github.com>
2020-06-04 16:28:19 +0700

    new lession java Swing

---
+++ sem2/ADF-II/JAVA2_BT/demoforfun/src/demoforfun/Demoforfun.java
@@ -0,0 +1,13 @@
+
+package demoforfun;
+
+
+public class Demoforfun {
+
+  
+    public static void main(String[] args) {
+        String a = new String("aalslsls");
+        System.out.println(a);
+    }
+    
+}
