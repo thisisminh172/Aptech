4aea950b638d9e169493a6634f16b76d165120bb
thisisminh172 <thisisminh172@gmail.com>
2020-09-19 07:54:44 +0700

    add lession

---
+++ sem2/ap-assignments/assignments/ass-4/ATMAccount.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class ATMAccount
+    {
+        private string accNo;
+        private int balance;
+
+        public string pAccNo
+        {
+            get { return accNo; }
+            set
+            {
+                if (Regex.IsMatch(value, "^ACC-\\d{5}$"))
+                {
+                    accNo = value;
+                }
+                else
+                {
+                    throw new Exception("Account number format is wrong [ACC-xxxxx] x is decimal");
+                }
+            }
+        }
+
+        public int pDeposit
+        {
+            set
+            {
+                if (value >= 100)
+                {
+                    balance += value;
+                }
+                else
+                {
+                    throw new Exception("The amount must be >= 100");
+                }
+            }
+        }
+
+        public int pWithdraw
+        {
+            set
+            {
+                if(value>= 100 && value < balance)
+                {
+                    balance -= value;
+                }
+                else
+                {
+                    throw new Exception("The amount must be >= 100 and < balance");
+                }
+            }
+        }
+
+        public int pBalance
+        {
+            get { return balance; }
+        }
+
+        public void input()
+        {
+            while (true)
+            {
+                try 
+                { 
+                    Console.Write("Input acc number: ");
+                    pAccNo = Console.ReadLine().Trim();
+                    break;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Deposit amount[>=100]: ");
+                    pDeposit = int.Parse(Console.ReadLine().Trim());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return string.Format("accNo: {0},balance: {1}", pAccNo,pBalance);
+        }
+
+        public ATMAccount()
+        {
+            input();
+        }
+
+    }
+}
---
+++ sem2/ap-assignments/assignments/ass-4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banking;
+
+namespace ass_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program m = new Program();
+            m.menu();
+            Console.ReadKey();
+        }
+        ATMAccount Acc;
+        int Amount;
+        public void menu()
+        {
+            string op = "";
+            Acc = new ATMAccount();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("---MENU---");
+                Console.WriteLine("1. Deposit Cash");
+                Console.WriteLine("2. Withdraw Cash");
+                Console.WriteLine("3. Balance");
+                Console.WriteLine("4. Exit");
+                op = Console.ReadLine().Trim();
+                switch (op)
+                {
+                    case "1":
+                        Console.Write("Enter the amount you want to deposit: ");
+                        Amount = int.Parse(Console.ReadLine().Trim());
+                        Acc.pDeposit = Amount;
+                        break;
+                    case "2":
+                        Console.Write("Enter the amount you want to withdraw: ");
+                        Amount = int.Parse(Console.ReadLine().Trim());
+                        Acc.pWithdraw = Amount;
+                        break;
+                    case "3":
+                        Console.WriteLine($"Your account balance: { Acc.pBalance}");
+                        break;
+                    case "4":
+                        Console.WriteLine("exit program");
+                        return;
+                    default:
+                        Console.WriteLine("invalid choice");
+                        break;
+                }
+
+                Console.Write(" Bam phim bat ky de tiep tuc chuong trinh...");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
