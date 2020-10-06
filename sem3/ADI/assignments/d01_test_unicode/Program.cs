fdc8fb50769bbd867d0a9d5d08799e7c77e56ced
thisisminh172 <thisisminh172@gmail.com>
2020-10-06 11:53:35 +0700

    d01

---
+++ sem3/ADI/assignments/d01_test_unicode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d01_test_unicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.Green;
+            string fname;
+            Console.Write("Nhập tên của bạn: ");
+            fname = Console.ReadLine();
+            Console.WriteLine("Tên của bạn là: "+fname);
+            Console.ReadKey();
+
+        }
+    }
+}
