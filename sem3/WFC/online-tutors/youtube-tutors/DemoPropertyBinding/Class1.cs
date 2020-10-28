133fcaf3362df69ad20a385df95689a5b56a4ebf
thisisminh172 <thisisminh172@gmail.com>
2020-10-28 17:30:35 +0700

    ok

---
+++ sem3/WFC/online-tutors/youtube-tutors/DemoPropertyBinding/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPropertyBinding
+{
+    class Class1 : INotifyPropertyChanged
+    {
+        private string _myStr;
+        public string MyStr {
+            get { return _myStr; }
+            set
+            {
+                if (value == _myStr) return;
+                _myStr = value;
+                OnPropertyChanged("MyStr");
+            }
+            }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ sem3/WFC/online-tutors/youtube-tutors/DemoPropertyBinding/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoPropertyBinding
+{
+    public partial class Form1 : Form
+    {
+        private Class1 _class1 = new Class1();
+        public Form1()
+        {
+            InitializeComponent();
+            //Binding binding = new Binding("Text", textBox2, "Text",true,DataSourceUpdateMode.OnPropertyChanged);
+
+            //textbox1 is datataget, textbox2 is datasource
+            //textBox1.DataBindings.Add(binding);
+            textBox1.DataBindings.Add("Text", _class1, "MyStr", true, DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _class1.MyStr = "HEy";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Text = _class1.MyStr;
+        }
+    }
+}
